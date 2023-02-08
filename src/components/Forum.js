0a2810cc4e6e58419bee1e7906dcb6d4afb86ebc
Merrick <merrickneo@gmail.com>
2023-02-08 21:46:40 +0800

    Add Home, About and Forum components

---
+++ src/components/Forum.js
@@ -0,0 +1,15 @@
+// Forum.js
+
+import React, { Component } from 'react';
+
+class Forum extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Forum</h2>
+        </div>
+    );
+  }
+}
+
+export default Forum;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ProvideAuth } from "./hooks/useAuth";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <ProvideAuth>
       <App />
     </ProvideAuth>
+    </BrowserRouter>
   </React.StrictMode>
 );
