aa8ada5ff95a7c0e43b0908589d9ffb83884ac3c
Merrick <merrickneo@gmail.com>
2023-02-09 01:48:04 +0800

    Remove image in about.js

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -1,16 +1,12 @@
 // About.js
 import React, { Component } from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
-import Login from "../Login.js";
-import Test from "../Test.js";
-import Forum from "./Forum.js";
 import innovateLogo from './resources/imgs/innovate.jpg';
 
 
 class About extends Component {
   render() {
     return (
-        <img src={innovateLogo}/>
+        <div>hih</div>
     );
   }
 }
