0ad64da401f12c1208a23a8a52976c6a94f3b67f
kavantan <kavantan10@gmail.com>
2023-02-08 21:18:35 +0800

    Create placeholder login feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useAuth } from "./hooks/useAuth.js";
+
 function App() {
-  return <div>Hello World!</div>;
+  const { user, signout, signInWithGoogle } = useAuth();
+  return (
+    <div>
+      {user ? (
+        <button onClick={signout}>Sign out</button>
+      ) : (
+        <>
+          <button onClick={signInWithGoogle}>Login with Google</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
