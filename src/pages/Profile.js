c4ce2d22ff6c8112728ae042ad1a80a991bc275e
kavantan <kavantan10@gmail.com>
2023-02-09 17:18:21 +0800

    Integrate user data from database to allow profile editing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { getDocs, query } from "firebase/firestore";
+import { usersCollectionRef } from "../config/firebase.collections";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useAuth();
+  const [usersData, setUsersData] = useState([]);
+
+  const getUsers = () => {
+    const q = query(usersCollectionRef);
+    getDocs(q)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setUsersData(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  useEffect(() => {
+    getUsers();
+  });
+
+  return (
+    <div>
+      {usersData.map((userData) => {
+        return userData.id === user.uid && <>{userData.data.name}</>;
+      })}
+    </div>
+  );
 };
 
 export default Profile;
