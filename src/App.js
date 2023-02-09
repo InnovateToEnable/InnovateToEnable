071e9b28a3ef9b1de9e84bc175417c536ad4a45f
kavantan <kavantan10@gmail.com>
2023-02-09 17:03:39 +0800

    Add placeholder pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import About from "./pages/About";
+import Forum from "./pages/Forum";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Test from "./pages/Test";
+import Profile from "./pages/Profile";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/test" element={<Test />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/forum" element={<Forum />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
