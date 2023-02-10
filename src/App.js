99734f80f18a7ed3ab61d6816dd7348c2650f90d
kavantan <kavantan10@gmail.com>
2023-02-10 19:57:09 +0800

    Install material-ui and add placeholder appbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./pages/About";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import EditProfile from "./pages/EditProfile";
 import Forum from "./pages/Forum";
 import Home from "./pages/Home";
@@ -10,16 +11,19 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/editprofile" element={<EditProfile />} />
-      <Route path="/forum" element={<Forum />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/users/:dir" element={<UsersProfile />}></Route>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <div>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:dir" element={<UsersProfile />}></Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
