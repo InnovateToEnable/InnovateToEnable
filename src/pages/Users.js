eec61d39caecea0bd882f438d3abac0e42acea8a
kavantan <kavantan10@gmail.com>
2023-02-10 22:19:59 +0800

    Add users page with rerouting to users' profiles

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,9 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getDocs, query } from "firebase/firestore";
+import { usersCollectionRef } from "../config/firebase.collections";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
+  const [usersData, setUsersData] = useState([]);
+
+  let navigate = useNavigate();
+
+  const getUsers = () => {
+    const q = query(usersCollectionRef);
+    getDocs(q)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setUsersData(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  useEffect(() => {
+    getUsers();
+  });
+
   return (
     <div>
-      <h1>Users</h1>
+      {usersData.map((user) => {
+        return (
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(user.data.dir)}
+          >
+            View Profile of: {user.data.name}
+          </div>
+        );
+      })}
     </div>
   );
 };
